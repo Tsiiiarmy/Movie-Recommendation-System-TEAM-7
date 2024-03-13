186d5bc4bdd4f85bd1e52aa095694511d44f5897
Antiaastu <136442178+Antiaastu@users.noreply.github.com>
2024-03-13 23:11:02 +0300

    some changes

--- Team-7-Movies/src/pages/WishPage.jsx
+++ Team-7-Movies/src/pages/WishPage.jsx
@@ -1,9 +1,9 @@
-import "../assets/wishlist.css";
+import React, { useState } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+
 
 const WishPage = () => {
   const [search, setSearch] = useState("");
@@ -13,33 +13,9 @@ const WishPage = () => {
   const isMounted = React.useRef(true);
   const [show, setShow] = useState("search-results");
   const [watchlist, setWatchList] = useState([]);
-  const apiKey = import.meta.env.VITE_RAPIDAPI_KEY
+  const apiKey = import.meta.env.VITE_RAPIDAPI_KEY;
+
   const fetchMovies = async () => {
-
-    // const moviesArray = [
-    //   {
-    //     id: 1,
-    //     title: "Fast X",
-    //     image: "images/fastx.jpg",
-    //     year: 2023,
-    //     isFavourite: false,
-    //   },
-    //   {
-    //     id: 2,
-    //     title: "The Godfather",
-    //     image: "images/gof.jpg",
-    //     year: 1972,
-    //     isFavourite: false,
-    //   },
-    //   {
-    //     id: 3,
-    //     title: "Spider man HomeComing",
-    //     image: "images/sp.jpg",
-    //     year: 2018,
-    //     isFavourite: false,
-    //   },
-    // ];
-
     const options = {
       method: "GET",
       url: "https://imdb8.p.rapidapi.com/title/find",
@@ -53,26 +29,15 @@ const WishPage = () => {
     try {
       const response = await axios.request(options);
       if (response.data.results !== undefined) {
-        console.log(response.data.results);
         setMovieData(response.data.results);
         setisFetch(false);
         setShow("search-results-show");
       }
-      // const filterdList = searchedValue
-      //   ? moviesArray.filter((item) =>
-      //       item.title.toLowerCase().includes(searchedValue.toLowerCase())
-      //     )
-      //   : false;
-      // if (filterdList) {
-      //   console.log(searchedValue);
-      //   setMovieData(filterdList);
-      //   setisFetch(false);
-      //   setShow("search-results-show");
-      // }
     } catch (error) {
       console.error(error);
     }
   };
+
   const handleNewItem = (item) => {
     const isObjectInList = watchlist.some((obj) => obj.id === item.id);
     if (isObjectInList) {
@@ -95,27 +60,36 @@ const WishPage = () => {
       setWatchList(newWatchList);
     }
   };
+
   const handleRemoveItem = (item) => {
-    const newWatchList = watchlist.filter(
-      (movie) => movie.title !== item.title
-    );
+    const newWatchList = watchlist.filter((movie) => movie.title !== item.title);
     const updatedMovieList = movieData.map((movie) =>
       movie.id === item.id ? { ...movie, isFavourite: false } : movie
     );
     setMovieData(updatedMovieList);
     setWatchList(newWatchList);
   };
-  useEffect(() => {
+
+  React.useEffect(() => {
     if (!isMounted.current) {
       isMounted.current = true;
     } else {
-      console.log("sadfghjk");
       fetchMovies();
     }
   }, [searchedValue]);
 
   return (
     <div className="wish-list-container">
+      <div className="navbar">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Favorite</li>
+          <li>Contact</li>
+          <li>Join Us</li>
+
+        </ul>
+      </div>
       <div className="wish-list-header">
         <div className="wish-header-content">
           <div className="header-title">
@@ -139,30 +113,36 @@ const WishPage = () => {
         </div>
       </div>
       <Watchlist watchlist={watchlist} handleRemoveItem={handleRemoveItem} />
+      <div className="footer">
+        <p>&copy; 2024 Movie Recommendation System</p>
+      </div>
     </div>
   );
 };
+
 const SearchForm = ({ search, setSearch, setSearchValue, setShow }) => {
   return (
     <div className="search-flex">
       <form
         onSubmit={(e) => {
-          e.preventDefault(),
-            setSearchValue(search),
-            setShow("search-results-show");
+          e.preventDefault();
+          setSearchValue(search);
+          setShow("search-results-show");
         }}
       >
         <input
           className="search-movie"
           placeholder="what do you want to watch"
           type="text"
+          value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <button className="search-btn"> Search</button>
+        <button className="search-btn">Search</button>
       </form>
     </div>
   );
 };
+
 const SearchCard = ({
   show,
   isfetching,
@@ -180,14 +160,14 @@ const SearchCard = ({
         ></i>
       </div>
       {isfetching ? (
-        <img src="images/loading.gif" />
+        <img src="images/loading.gif" alt="Loading..." />
       ) : (
         movieData.map((movie) => (
           <div key={movie.id} className="search-card">
             <img
               className="search-image"
               src={movie.image ? movie.image.url : "/images/default.png"}
-              // src={movie.image}
+              alt="Movie Poster"
             />
             <span className="search-title">
               {movie.title} <br /> {movie.year}
@@ -196,11 +176,11 @@ const SearchCard = ({
               style={{ marginLeft: "auto" }}
               onClick={() => handleNewItem(movie)}
             >
-              <i
-                className="favourite-btn fas fa-heart"
+              <FontAwesomeIcon
+                className="favourite-btn"
+                icon={faHeart}
                 style={{ color: movie.isFavourite ? "red" : "" }}
-                aria-hidden="true"
-              ></i>
+              />
             </span>
           </div>
         ))
@@ -208,6 +188,7 @@ const SearchCard = ({
     </div>
   );
 };
+
 const Watchlist = ({ watchlist, handleRemoveItem }) => {
   return (
     <div className="movies-container">
@@ -217,11 +198,10 @@ const Watchlist = ({ watchlist, handleRemoveItem }) => {
       <div className="movie-list">
         {watchlist.length > 0 ? (
           watchlist.map((movie) => (
-            <div className="movie-card">
+            <div className="movie-card" key={movie.id}>
               <>
                 <img
                   className="wishlist-image"
-                  // src={movie.image}
                   src={movie.image}
                   alt={`Movie Poster for ${movie.title}`}
                 />
@@ -246,4 +226,5 @@ const Watchlist = ({ watchlist, handleRemoveItem }) => {
     </div>
   );
 };
+
 export default WishPage;
