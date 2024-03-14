a7b20029cee6bb9efaadae09899efc95376f5b49
amanuelcm27 <amanuelfirew27@gmail.com>
2024-03-14 20:52:08 +0300

    contact as page and adding it to the routes

--- Team-7-Movies/src/App.jsx
+++ Team-7-Movies/src/App.jsx
@@ -4,6 +4,7 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import "./App.css";
 import WishPage from "./pages/WishPage";
 import MovieDetail from "./pages/MovieDetail";
+import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MovieDetail/>}></Route>
         <Route path="/wishlist" element={<WishPage/>}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </Router>
     </>
---
+++ Team-7-Movies/src/pages/Contact.jsx
@@ -0,0 +1,28 @@
+import "../assets/contact.css";
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <div className="contact-overlay">
+        <div className="contact-grid">
+          <div className="contact-card">
+            <div className="contact-detail">
+              <h1 className="contact-head">Contact</h1>
+              <form className="contact-form">
+                <div className="input-card">
+                    <input className="form-input" placeholder="Your name" type="text" />
+                    <input className="form-input"  placeholder="Your email" type="email" />
+                    <textarea className="form-input"  placeholder="Your Message ... " type="text" /> 
+                </div>
+                <div className="submit-card">
+                   <button className="contact-submit"> <i style={{paddingRight:"15px"}} className="fas fa-paper-plane"></i>Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
