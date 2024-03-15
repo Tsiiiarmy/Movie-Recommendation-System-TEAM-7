1ee34ebb7b95f17d306d04c193492823d9fa68fc
Anti G <128841483+anteneh83@users.noreply.github.com>
2024-03-15 10:40:42 -0700

    creating beautifull navbar

--- Team-7-Movies/src/App.jsx
+++ Team-7-Movies/src/App.jsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import "./App.css";
 import WishPage from "./pages/WishPage";
 import JoinNow from "./pages/JoinNow";
+import NavBar from "./pages/NavBar";
 function App() {
 
   return (
     <>
       {/* <WishPage/> */}
+      <NavBar />
       <JoinNow />
     </>
   );
---
+++ Team-7-Movies/src/pages/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../assets/NavBar.css'
+import movie from '../../public/images/movie.svg'
+
+const NavBar = () => {
+  return (
+   <div className="navbar">
+    <div className="movie-title">
+        <img src={movie} alt="movie" className='logo'/>
+        <h1>TEAM7MOVIES</h1>
+    </div>
+    <div className="menu-btn">
+        <ul className='navbar-menu'>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>FAVORITE</li>
+            <li>CONTACT</li>
+        </ul>
+        <button className='btn'>Join Now</button>
+    </div>
+   </div>
+  )
+}
+
+export default NavBar
