bc5d84e53c79dbb1c7bcdd811bf450c47a875e1b
Ayub Sufian <ayubsufiane@gmail.com>
2024-03-15 15:18:39 +0300

    Update App.jsx

--- Team-7-Movies/src/App.jsx
+++ Team-7-Movies/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import "./assets/wishlist.css";
-import '@fortawesome/fontawesome-free/css/all.css';
+import "@fortawesome/fontawesome-free/css/all.css";
 
 import "./App.css";
 import WishPage from "./pages/WishPage";
 function App() {
-
   return (
     <>
-      <WishPage/>
+      <WishPage />
     </>
   );
 }
